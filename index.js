2e9719d226c006dfaa01a1ae2c23fd11589171df
Wojciech Maj (Partner) <wojciech.maj@motorolasolutions.com>
2016-08-01 16:03:40 +0200

    Changed default loading message

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ var Pdf = React.createClass({
       });
       return (React.createElement('canvas', { ref: 'pdfCanvas' }));
     }
-    return (this.props.loading || React.createElement("div", null, "Loading pdf...."));
+    return (this.props.loading || React.createElement('div', null, 'Loading PDF…'));
   },
   _onDocumentComplete: function (pdf) {
     if (!this.isMounted()) return;
