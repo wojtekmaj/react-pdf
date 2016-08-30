5d8a8c996eee220153e5cbc7fe74f92a26536095
m.igrachev <milen.igrachev@gmail.com>
2016-08-30 15:43:15 +0300

    Correcting: 1. Problem with PropTypes of file property. 2. A problem with updating the component file prop from a consumer.

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ var ReactDOM = require('react-dom');
 var Pdf = React.createClass({
   displayName: 'React-PDF',
   propTypes: {
-    file: React.PropTypes.string,
+    file: React.PropTypes.oneOfType([
+      React.PropTypes.file,
+      React.PropTypes.string
+    ]),
     content: React.PropTypes.string,
     page: React.PropTypes.number,
     scale: React.PropTypes.number,
@@ -51,6 +54,7 @@ var Pdf = React.createClass({
   },
   componentWillReceiveProps: function(newProps) {
     if ((newProps.file && newProps.file !== this.props.file) || (newProps.content && newProps.content !== this.props.content)) {
+      this.setState({page: null});
       this._loadPDFDocument(newProps);
     }
     if (!!this.state.pdf && !!newProps.page && newProps.page !== this.props.page) {
