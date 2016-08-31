d5631da2cbfc8bcd4771acc78609c6d1e2e1b485
Wojciech Maj (Partner) <wojciech.maj@motorolasolutions.com>
2016-08-31 10:36:57 +0200

    Updated built version

--- es5/index.js
+++ es5/index.js
@@ -12,10 +12,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -159,11 +155,12 @@ var ReactPDF = function (_React$Component) {
                         canvas.height = viewport.height;
                         canvas.width = viewport.width;
 
+                        context.clearRect(0, 0, canvas.width, canvas.height);
+
                         var renderContext = {
                             canvasContext: context,
                             viewport: viewport
                         };
-
                         self.state.page.render(renderContext);
                     }
                 });
