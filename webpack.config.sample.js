16e38050331adf0ab50fe5e43013b2d9d02e05b9
Wojciech Maj <kontakt@wojtekmaj.pl>
2016-11-16 00:15:15 +0100

    Minor webpack config fixes

--- webpack.config.sample.js
+++ webpack.config.sample.js
@@ -34,11 +34,10 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV': JSON.stringify("production"),
-            }
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
         }),
         new webpack.optimize.UglifyJsPlugin(),
-    ]
+    ],
 };
-
--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,5 +1,3 @@
-var webpack = require('webpack');
-
 module.exports = {
     context: __dirname,
     devtool: 'source-map',
@@ -34,4 +32,3 @@ module.exports = {
         ],
     },
 };
-
