238af95c70f1e136fc45d9a168be0d7beada09de
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-03-03 23:47:17 +0100

    Change indentation to 2 spaces & remove ESLint Airbnb rules overwriting

--- webpack.config.sample.js
+++ webpack.config.sample.js
@@ -1,51 +1,51 @@
 const webpack = require('webpack');
 
 module.exports = {
-    context: __dirname,
-    entry: [
-        './src-sample/index.html',
-        './src-sample/sample.jsx',
-        './src-sample/sample.pdf',
-    ],
-    output: {
-        path: './sample',
-        filename: 'sample.js',
-        chunkFilename: 'sample.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: 'file-loader?name=[name].[ext]',
-            },
-            {
-                test: /\.pdf$/,
-                use: 'url-loader',
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
+  context: __dirname,
+  entry: [
+    './src-sample/index.html',
+    './src-sample/sample.jsx',
+    './src-sample/sample.pdf',
+  ],
+  output: {
+    path: './sample',
+    filename: 'sample.js',
+    chunkFilename: 'sample.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'file-loader?name=[name].[ext]',
+      },
+      {
+        test: /\.pdf$/,
+        use: 'url-loader',
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
         ],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production'),
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
     ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 };
