ddd15c420c4251a53dae3b3d7f31a823f958728b
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-12 03:13:56 +0200

    Update compiled files

--- es5/react-pdf.js
+++ es5/react-pdf.js
@@ -29,9 +29,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 require('pdfjs-dist/web/compatibility');
-/**
- * Passing string won't work as a separate module is required anyway
- */
+
 _pdfjsDist2.default.PDFJS.workerSrc = 'pdf.worker.js';
 
 var ReactPDF = function (_Component) {
