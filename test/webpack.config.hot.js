80ed1e67175d9a87ae81cd2c6e0eb861f193a55a
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-16 03:16:56 +0200

    Create separate configs for HMR and normal build of test suite

--- test/webpack.config.hot.js
+++ test/webpack.config.hot.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -6,10 +7,11 @@ module.exports = {
   devtool: 'source-map',
   entry: [
     'babel-polyfill',
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
     './index',
   ],
   output: {
-    path: path.join(__dirname, 'build'),
     filename: '[name].bundle.js',
   },
   resolve: {
@@ -33,6 +35,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: [
+          'react-hot-loader/webpack',
           'babel-loader',
         ],
       },
@@ -43,5 +46,14 @@ module.exports = {
       { from: './index.html' },
       { from: './test.pdf' },
     ]),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true, // respond to 404s with index.html
+    hot: true, // enable HMR on the server
+  },
 };
