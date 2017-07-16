28457fc75587a6048f71043a795c29eec58d5a03
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-16 21:35:17 +0200

    Fixe pages rendered sometimes being blurry due to the lack of rounding width/height down

--- src/Page.jsx
+++ src/Page.jsx
@@ -184,8 +184,8 @@ export default class Page extends Component {
     canvas.width = viewport.width;
     canvas.height = viewport.height;
 
-    canvas.style.height = `${viewport.height / pixelRatio}px`;
-    canvas.style.width = `${viewport.width / pixelRatio}px`;
+    canvas.style.height = `${Math.floor(viewport.height / pixelRatio)}px`;
+    canvas.style.width = `${Math.floor(viewport.width / pixelRatio)}px`;
 
     const canvasContext = canvas.getContext('2d');
 
