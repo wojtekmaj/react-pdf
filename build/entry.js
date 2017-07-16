f6ec00b49d6f45b240a2252aeeabe955f2f1dc7f
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-16 03:14:48 +0200

    Fix including babel-polyfill multiple times

--- build/entry.js
+++ build/entry.js
@@ -15,7 +15,6 @@ var _Page2 = _interopRequireDefault(_Page);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-require('babel-polyfill');
 require('pdfjs-dist');
 require('pdfjs-dist/web/compatibility');
 
--- build/entry.webpack.js
+++ build/entry.webpack.js
@@ -22,7 +22,6 @@ if ((0, _util.isLocalFileSystem)()) {
   console.warn('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
 }
 
-require('babel-polyfill');
 require('pdfjs-dist/webpack');
 require('pdfjs-dist/web/compatibility');
 
