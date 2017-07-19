6468d0328392c8204698e9bb15b08e505d0edc88
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-20 00:53:18 +0200

    Add workerSrc setting to non-webpack entry page to stop PDF.js from attempting to load worker using the name of the last script in the head

--- src/entry.js
+++ src/entry.js
@@ -2,8 +2,10 @@ import Document from './Document';
 import Outline from './Outline';
 import Page from './Page';
 
-require('pdfjs-dist');
+const pdfjs = require('pdfjs-dist');
 require('pdfjs-dist/web/compatibility');
+
+pdfjs.PDFJS.workerSrc = 'pdf.worker.js';
 
 export {
   Document,
