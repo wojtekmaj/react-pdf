eee790ecb75c10ddff2efd2fcb1c35a22ceb569f
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-20 00:53:37 +0200

    Add local file system warning on default entry file

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,13 @@ import Document from './Document';
 import Outline from './Outline';
 import Page from './Page';
 
+import { isLocalFileSystem, warnOnDev } from './shared/util';
+
+if (isLocalFileSystem) {
+  // eslint-disable-next-line no-console
+  warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
+}
+
 const pdfjs = require('pdfjs-dist');
 require('pdfjs-dist/web/compatibility');
 
