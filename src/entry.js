f62c4e893a13e4ceb3de538022ab148246635edf
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-19 23:31:26 +0200

    Add babel-polyfill to the code if it was not defined beforehand

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,12 @@ import Document from './Document';
 import Outline from './Outline';
 import Page from './Page';
 
+// eslint-disable-next-line no-underscore-dangle
+if (!window._babelPolyfill) {
+  // eslint-disable-next-line global-require
+  require('babel-polyfill');
+}
+
 require('pdfjs-dist');
 require('pdfjs-dist/web/compatibility');
 
--- src/entry.webpack.js
+++ src/entry.webpack.js
@@ -4,6 +4,12 @@ import Page from './Page';
 
 import { isLocalFileSystem, warnOnDev } from './shared/util';
 
+// eslint-disable-next-line no-underscore-dangle
+if (!window._babelPolyfill) {
+  // eslint-disable-next-line global-require
+  require('babel-polyfill');
+}
+
 if (isLocalFileSystem) {
   // eslint-disable-next-line no-console
   warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
