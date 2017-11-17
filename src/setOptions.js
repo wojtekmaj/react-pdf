f5be7de9f74e2c3034b0ba84005d914a38cd42f5
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-11-17 01:51:04 +0100

    Add setOptions method for easily setting PDFJS options

---
+++ src/setOptions.js
@@ -0,0 +1,21 @@
+const allowedProperties = [
+  'cMapUrl',
+  'cMapPacked',
+  'disableWorker',
+  'workerSrc',
+];
+
+const makeSetOptions = pdfjs => (options) => {
+  if (!(options instanceof Object)) {
+    return;
+  }
+
+  /* eslint-disable no-param-reassign */
+  Object.keys(options)
+    .filter(property => allowedProperties.includes(property))
+    .forEach((property) => {
+      pdfjs.PDFJS[property] = options[property];
+    });
+};
+
+export default makeSetOptions;
