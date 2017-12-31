e644de0f98c9eb4e33ae13df18ac5807e89dd8c2
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-12-31 19:34:24 +0100

    Update test environment

--- enzyme.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { configure } = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-configure({ adapter: new Adapter() });
