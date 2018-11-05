b47ab2b4b256e468a09ab0473b1a4ede771da217
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-05 16:49:31 +0100

    Use DocumentProxy API to access numPages
    https://mozilla.github.io/pdf.js/api/draft/PDFDocumentProxy.html

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -74,7 +74,7 @@ export const isPageIndex = (props, propName, componentName) => {
       return new Error(`Expected \`${propName}\` to be greater or equal to 0.`);
     }
 
-    const { numPages } = pdf.pdfInfo;
+    const { numPages } = pdf;
 
     if (pageIndex + 1 > numPages) {
       return new Error(`Expected \`${propName}\` to be less or equal to ${numPages - 1}.`);
@@ -103,7 +103,7 @@ export const isPageNumber = (props, propName, componentName) => {
       return new Error(`Expected \`${propName}\` to be greater or equal to 1.`);
     }
 
-    const { numPages } = pdf.pdfInfo;
+    const { numPages } = pdf;
 
     if (pageNumber > numPages) {
       return new Error(`Expected \`${propName}\` to be less or equal to ${numPages}.`);
