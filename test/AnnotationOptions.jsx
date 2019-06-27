ca5b1a4df8d0630cb27975163d3a45a5bfbe47c5
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-06-27 17:22:28 +1000

    Rewrite all Test suite options to functional components

--- test/AnnotationOptions.jsx
+++ test/AnnotationOptions.jsx
@@ -1,68 +1,66 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class AnnotationOptions extends PureComponent {
-  onExternalLinkTargetChange = (event) => {
-    const { setState } = this.props;
+export default function AnnotationOptions({
+  externalLinkTarget,
+  setState,
+}) {
+  function onExternalLinkTargetChange(event) {
     const { value } = event.target;
 
     setState({ externalLinkTarget: value !== 'unset' ? value : null });
   }
 
-  render() {
-    const { externalLinkTarget } = this.props;
+  return (
+    <fieldset id="viewoptions">
+      <legend htmlFor="viewoptions">
+        Annotation options
+      </legend>
 
-    return (
-      <fieldset id="viewoptions">
-        <legend htmlFor="viewoptions">
-          Annotation options
-        </legend>
-
-        <label htmlFor="externalLinkTarget">
-          External link target
+      <label htmlFor="externalLinkTarget">
+        External link target
+      </label>
+      <div>
+        <input
+          checked={!externalLinkTarget || (externalLinkTarget === 'unset')}
+          id="targetUnset"
+          name="externalLinkTarget"
+          onChange={onExternalLinkTargetChange}
+          type="radio"
+          value="unset"
+        />
+        <label htmlFor="targetUnset">
+          Unset
         </label>
-        <div>
-          <input
-            checked={!externalLinkTarget || (externalLinkTarget === 'unset')}
-            id="targetUnset"
-            name="externalLinkTarget"
-            onChange={this.onExternalLinkTargetChange}
-            type="radio"
-            value="unset"
-          />
-          <label htmlFor="targetUnset">
-            Unset
-          </label>
-        </div>
-        <div>
-          <input
-            checked={externalLinkTarget === '_self'}
-            id="targetSelf"
-            name="externalLinkTarget"
-            onChange={this.onExternalLinkTargetChange}
-            type="radio"
-            value="_self"
-          />
-          <label htmlFor="targetSelf">
-            _self
-          </label>
-        </div>
-        <div>
-          <input
-            checked={externalLinkTarget === '_blank'}
-            id="targetBlank"
-            name="externalLinkTarget"
-            onChange={this.onExternalLinkTargetChange}
-            type="radio"
-            value="_blank"
-          />
-          <label htmlFor="targetBlank">
-            _blank
-          </label>
-        </div>
-      </fieldset>
-    );
-  }
+      </div>
+      <div>
+        <input
+          checked={externalLinkTarget === '_self'}
+          id="targetSelf"
+          name="externalLinkTarget"
+          onChange={onExternalLinkTargetChange}
+          type="radio"
+          value="_self"
+        />
+        <label htmlFor="targetSelf">
+          _self
+        </label>
+      </div>
+      <div>
+        <input
+          checked={externalLinkTarget === '_blank'}
+          id="targetBlank"
+          name="externalLinkTarget"
+          onChange={onExternalLinkTargetChange}
+          type="radio"
+          value="_blank"
+        />
+        <label htmlFor="targetBlank">
+          _blank
+        </label>
+      </div>
+    </fieldset>
+  );
 }
 
 AnnotationOptions.propTypes = {
--- test/LayerOptions.jsx
+++ test/LayerOptions.jsx
@@ -1,77 +1,68 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class LayerOptions extends PureComponent {
-  onRenderAnnotationLayerChange = (event) => {
-    const { setState } = this.props;
-
+export default function LayerOptions({
+  renderAnnotationLayer,
+  renderInteractiveForms,
+  renderTextLayer,
+  setState,
+}) {
+  function onRenderAnnotationLayerChange(event) {
     setState({ renderAnnotationLayer: event.target.checked });
   }
 
-  onRenderInteractiveFormsChange = (event) => {
-    const { setState } = this.props;
-
+  function onRenderInteractiveFormsChange(event) {
     setState({ renderInteractiveForms: event.target.checked });
   }
 
-  onRenderTextLayersChange = (event) => {
-    const { setState } = this.props;
-
+  function onRenderTextLayersChange(event) {
     setState({ renderTextLayer: event.target.checked });
   }
 
-  render() {
-    const {
-      renderAnnotationLayer,
-      renderInteractiveForms,
-      renderTextLayer,
-    } = this.props;
+  return (
+    <fieldset id="viewoptions">
+      <legend htmlFor="viewoptions">
+        Layer options
+      </legend>
 
-    return (
-      <fieldset id="viewoptions">
-        <legend htmlFor="viewoptions">
-          Layer options
-        </legend>
+      <div>
+        <input
+          id="renderTextLayer"
+          type="checkbox"
+          checked={renderTextLayer}
+          onChange={onRenderTextLayersChange}
+        />
+        <label htmlFor="renderTextLayer">
+          Render text layer
+        </label>
+      </div>
 
-        <div>
-          <input
-            id="renderTextLayer"
-            type="checkbox"
-            checked={renderTextLayer}
-            onChange={this.onRenderTextLayersChange}
-          />
-          <label htmlFor="renderTextLayer">
-            Render text layer
-          </label>
-        </div>
+      <div>
+        <input
+          id="renderAnnotationLayer"
+          type="checkbox"
+          checked={renderAnnotationLayer}
+          onChange={onRenderAnnotationLayerChange}
+        />
+        <label htmlFor="renderAnnotationLayer">
+          Render annotation layer
+        </label>
+      </div>
 
-        <div>
-          <input
-            id="renderAnnotationLayer"
-            type="checkbox"
-            checked={renderAnnotationLayer}
-            onChange={this.onRenderAnnotationLayerChange}
-          />
-          <label htmlFor="renderAnnotationLayer">
-            Render annotation layer
-          </label>
-        </div>
-
-        <div>
-          <input
-            id="renderInteractiveForms"
-            type="checkbox"
-            checked={renderAnnotationLayer && renderInteractiveForms}
-            onChange={this.onRenderInteractiveFormsChange}
-            disabled={!renderAnnotationLayer}
-          />
-          <label htmlFor="renderInteractiveForms">
-            Render interactive forms
-          </label>
-        </div>
-      </fieldset>
-    );
-  }
+      <div>
+        <input
+          id="renderInteractiveForms"
+          type="checkbox"
+          checked={renderAnnotationLayer && renderInteractiveForms}
+          onChange={onRenderInteractiveFormsChange}
+          disabled={!renderAnnotationLayer}
+        />
+        <label htmlFor="renderInteractiveForms">
+          Render interactive forms
+        </label>
+      </div>
+    </fieldset>
+  );
 }
 
 LayerOptions.propTypes = {
