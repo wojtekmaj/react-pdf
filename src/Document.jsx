cf85645022a5b0ead2741491ea6e5a891c703b8c
Mathias Osmar <mathias_os@hotmail.com>
2019-07-26 01:25:10 -0300

    Moved import react-fast-compare to before import DocumentContext

--- src/Document.jsx
+++ src/Document.jsx
@@ -7,6 +7,7 @@ import makeEventProps from 'make-event-props';
 import makeCancellable from 'make-cancellable-promise';
 import mergeClassNames from 'merge-class-names';
 import pdfjs, { PDFDataRangeTransport } from 'pdfjs-dist';
+import isEqual from 'react-fast-compare';
 
 import DocumentContext from './DocumentContext';
 
@@ -31,8 +32,6 @@ import {
 } from './shared/utils';
 
 import { eventsProps, isClassName } from './shared/propTypes';
-
-import isEqual from 'react-fast-compare';
 
 export default class Document extends PureComponent {
   state = {
