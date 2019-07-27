fd4246afe871f4c70bd279bf0e60be0a28e92045
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-27 15:47:30 +0200

    Create ESM builds

--- copy-styles.js
+++ copy-styles.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
-fs.copyFile('src/Page/AnnotationLayer.css', 'dist/Page/AnnotationLayer.css', (error) => {
+fs.copyFile('src/Page/AnnotationLayer.css', 'dist/esm/Page/AnnotationLayer.css', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('AnnotationLayer.css copied successfully.');
+  console.log('AnnotationLayer.css copied successfully to ESM build.');
+});
+
+fs.copyFile('src/Page/AnnotationLayer.css', 'dist/umd/Page/AnnotationLayer.css', (error) => {
+  if (error) {
+    throw error;
+  }
+  // eslint-disable-next-line no-console
+  console.log('AnnotationLayer.css copied successfully to UMD build.');
 });
