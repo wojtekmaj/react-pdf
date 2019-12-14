24e58c7704fa86631cc3813ca2b59e926dd9d12f
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-12-14 21:34:13 +0100

    Change Message into functional component

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Message = ({ children, type }) => (
-  <div className={`react-pdf__message react-pdf__message--${type}`}>
-    {children}
-  </div>
-);
+export default function Message({ children, type }) {
+  return (
+    <div className={`react-pdf__message react-pdf__message--${type}`}>
+      {children}
+    </div>
+  );
 
 Message.propTypes = {
   children: PropTypes.node,
   type: PropTypes.oneOf(['error', 'loading', 'no-data']).isRequired,
 };
-
-export default Message;
