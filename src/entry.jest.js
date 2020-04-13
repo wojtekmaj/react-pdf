7cc9a94516784f894845c25ad1ed7d8cec03723e
Kyle Mellander <kylemellander@gmail.com>
2020-04-13 11:43:45 -0700

    use ES5 version of workers for specs
    The ES6 version requires support of ReadableStream causing issues.  
    Rather than using a polyfill or mocking this, we should just assume that 
    it has been compiled to a working syntax, which for the tests is ES5.

--- src/entry.jest.js
+++ src/entry.jest.js
@@ -1,9 +1,9 @@
-import pdfjs from 'pdfjs-dist';
+import pdfjs from 'pdfjs-dist/es5/build/pdf';
 import Document from './Document';
 import Outline from './Outline';
 import Page from './Page';
 
-import './pdf.worker.entry';
+import './pdf.worker.entry.jest';
 
 export {
   pdfjs,
---
+++ src/pdf.worker.entry.jest.js
@@ -0,0 +1,13 @@
+/**
+ * PDF.js Worker entry file.
+ *
+ * This file is identical to Mozilla's pdf.worker.entry.js, with one exception being placed inside
+ * this bundle, not theirs. This file can be safely removed and replaced with Mozilla's after the
+ * issue mentioned below has been resolved on Parcel's side.
+ * See: https://github.com/parcel-bundler/parcel/issues/670
+ */
+
+(typeof window !== 'undefined'
+  ? window
+  : {}
+).pdfjsWorker = require('pdfjs-dist/es5/build/pdf.worker.js');
