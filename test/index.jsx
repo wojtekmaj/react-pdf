868907fab8925c2ea58a87702b3fd32d46f05573
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-08-12 12:30:28 +0200

    Replace React-🔥-Loader with React Fast Refresh (#626)

--- test/index.jsx
+++ test/index.jsx
@@ -1,22 +1,9 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/no-unresolved
-import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import { render } from 'react-dom';
+
 import Test from './Test';
 
-const render = (Component) => {
-  ReactDOM.render(
-    <StrictMode>
-      <AppContainer>
-        <Component />
-      </AppContainer>
-    </StrictMode>,
-    document.getElementById('react-container'),
-  );
-};
-
-render(Test);
-
-if (module.hot) {
-  module.hot.accept('./Test', () => { render(Test); });
-}
+render(
+  <Test />,
+  document.getElementById('react-container'),
+);
