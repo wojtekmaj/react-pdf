216693c6c830d52233606a41739a4739e986402b
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-09-11 19:07:01 +0200

    Upgrade Webpack to v5

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash:8].js',
+    filename: '[name].[chunkhash:8].js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
