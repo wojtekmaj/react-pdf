deb413c07d51cfb2f4eb0d3c46fd76e5a8658798
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-10-14 17:32:40 +0200

    Add unit test for onRenderSuccess in PageCanvas

--- src/Page/PageCanvas.spec.jsx
+++ src/Page/PageCanvas.spec.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
+import { pdfjs } from '../entry.jest';
+
 import { PageCanvasInternal as PageCanvas } from './PageCanvas';
 
 import failingPage from '../../__mocks__/_failing_page';
 
-import { makeAsyncCallback, muteConsole, restoreConsole } from '../../test-utils';
+import {
+  loadPDF, makeAsyncCallback, muteConsole, restoreConsole,
+} from '../../test-utils';
+
+const pdfFile = loadPDF('./__mocks__/_pdf.pdf');
 
 /* eslint-disable comma-dangle */
 
 describe('PageCanvas', () => {
+  // Loaded page
+  let page;
+
+  beforeAll(async () => {
+    const pdf = await pdfjs.getDocument({ data: pdfFile.arrayBuffer }).promise;
+
+    page = await pdf.getPage(1);
+  });
+
   describe('loading', () => {
+    it('renders a page and calls onRenderSuccess callback properly', async () => {
+      const { func: onRenderSuccess, promise: onRenderSuccessPromise } = makeAsyncCallback();
+
+      const pageWithRendererMocked = {
+        ...page,
+        getAnnotations: () => {},
+        getTextContent: () => {},
+        getViewport: () => ({
+          width: 0,
+          height: 0,
+        }),
+        render: () => ({
+          promise: new Promise(resolve => resolve()),
+        }),
+      };
+
+      mount(
+        <PageCanvas
+          onRenderSuccess={onRenderSuccess}
+          page={pageWithRendererMocked}
+        />
+      );
+
+      expect.assertions(1);
+
+      await expect(onRenderSuccessPromise).resolves.toMatchObject({});
+    });
+
     it('calls onRenderError when failed to render canvas', async () => {
       const {
         func: onRenderError, promise: onRenderErrorPromise
@@ -26,6 +69,7 @@ describe('PageCanvas', () => {
       );
 
       expect.assertions(1);
+
       await expect(onRenderErrorPromise).resolves.toBeInstanceOf(Error);
 
       restoreConsole();
