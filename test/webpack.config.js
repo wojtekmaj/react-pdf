9278d585ea6cb78a12328d6d301031356c744ef1
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-27 12:11:17 +0100

    Enable remote debugging

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -89,7 +89,7 @@ module.exports = {
   devServer: {
     compress: true,
     historyApiFallback: true, // respond to 404s with index.html
-    host: 'localhost',
+    host: '0.0.0.0',
     hot: true, // enable HMR on the server
     port: 3000,
   },
