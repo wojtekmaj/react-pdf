cea7927e96bf74a00843356cc45e916d9d7cd8ae
lyqht <estee.tsw@gmail.com>
2021-09-06 23:43:35 +0800

    changed plugin node_env to use correct environment

--- sample/webpack5/webpack.config.js
+++ sample/webpack5/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
+        NODE_ENV: JSON.stringify(isProduction ? 'production' : 'development'),
       },
     }),
     new HtmlWebpackPlugin({
