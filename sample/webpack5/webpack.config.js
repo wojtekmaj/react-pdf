5c6b3a8756d931feaacbbd7a9da789a694b40e16
Estee Tey Siew Wen <estee.tsw@gmail.com>
2021-09-08 20:22:14 +0800

    Update webpack.config.js

--- sample/webpack5/webpack.config.js
+++ sample/webpack5/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(isProduction ? 'production' : 'development'),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
     new HtmlWebpackPlugin({
