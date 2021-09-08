7fca6c61f89a6fa6dfd9985a0e5221fbba459261
Estee Tey Siew Wen <estee.tsw@gmail.com>
2021-09-08 21:16:47 +0800

    Fix conflicting values for 'process.env.NODE_ENV' (#841)

--- sample/webpack4/webpack.config.js
+++ sample/webpack4/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
     new HtmlWebpackPlugin({
