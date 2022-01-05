1dd5915b9d9e312c2427a44f95457253f6d1f60f
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-01-05 14:39:21 +0100

    Add Parcel 2 sample

---
+++ sample/parcel2/Sample.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+import './Sample.less';
+
+const pdfFile = new URL('./sample.pdf', import.meta.url).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('npm:react-pdf/dist/esm/pdf.worker.entry.js', import.meta.url);
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+export default function Sample() {
+  const [file, setFile] = useState(pdfFile);
+  const [numPages, setNumPages] = useState(null);
+
+  function onFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>
+          {' '}
+          <input
+            onChange={onFileChange}
+            type="file"
+          />
+        </div>
+        <div className="Example__container__document">
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {
+              Array.from(
+                new Array(numPages),
+                (el, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                  />
+                ),
+              )
+            }
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
