1077ec56414e5d2d09c080755dd04ae5bd3870ef
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-01-19 20:59:33 +0100

    Update instructions on PDF.js worker
    Related to #912

--- src/pdf.worker.entry.js
+++ src/pdf.worker.entry.js
@@ -1,5 +1,5 @@
 /**
- * PDF.js Worker entry file.
+ * PDF.js worker entry file.
  *
  * This file is identical to Mozilla's pdf.worker.entry.js, with one exception being placed inside
  * this bundle, not theirs.
