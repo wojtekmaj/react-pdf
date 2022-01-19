60d382045f098166773667636527570fa1a757cc
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-01-19 10:36:16 +0100

    Remove unnecessary eslint-disable

--- src/entry.parcel.js
+++ src/entry.parcel.js
@@ -7,7 +7,6 @@ import Page from './Page';
 import { isLocalFileSystem, warnOnDev } from './shared/utils';
 
 if (isLocalFileSystem) {
-  // eslint-disable-next-line no-console
   warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
 }
 
