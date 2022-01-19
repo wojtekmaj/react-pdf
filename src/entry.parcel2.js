5466870c027d544716fb7897f99e1978ab252617
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-01-19 10:41:22 +0100

    Add Parcel 2 specific entry, update README, update Parcel 2 sample

---
+++ src/entry.parcel2.js
@@ -0,0 +1,20 @@
+import * as pdfjs from 'pdfjs-dist/legacy/build/pdf';
+
+import Document from './Document';
+import Outline from './Outline';
+import Page from './Page';
+
+import { isLocalFileSystem, warnOnDev } from './shared/utils';
+
+if (isLocalFileSystem) {
+  warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
+}
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('npm:pdfjs-dist/legacy/build/pdf.worker', import.meta.url);
+
+export {
+  pdfjs,
+  Document,
+  Outline,
+  Page,
+};
