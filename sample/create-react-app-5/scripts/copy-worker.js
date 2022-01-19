40e5b3cec3231ecd2705c795f1e7f7d8f170b008
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-01-19 21:19:21 +0100

    Add Create React App 5 sample (#923)

---
+++ sample/create-react-app-5/scripts/copy-worker.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
+const pdfWorkerPath = path.join(pdfjsDistPath, 'legacy', 'build', 'pdf.worker.js');
+
+const targetDir = 'public';
+const targetPath = path.join(targetDir, 'pdf.worker.js');
+
+// Ensure target directory exists
+fs.mkdirSync(targetDir, { recursive: true });
+
+// Copy file
+fs.copyFileSync(pdfWorkerPath, targetPath);
