5260aeaa7c9735a66fd19b1b47908f3947f1122f
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-09-27 22:11:38 +0200

    Rename umd to cjs

--- copy-styles.js
+++ copy-styles.js
@@ -8,12 +8,12 @@ fs.copyFile('src/Page/AnnotationLayer.css', 'dist/esm/Page/AnnotationLayer.css',
   console.log('AnnotationLayer.css copied successfully to ESM build.');
 });
 
-fs.copyFile('src/Page/AnnotationLayer.css', 'dist/umd/Page/AnnotationLayer.css', (error) => {
+fs.copyFile('src/Page/AnnotationLayer.css', 'dist/cjs/Page/AnnotationLayer.css', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('AnnotationLayer.css copied successfully to UMD build.');
+  console.log('AnnotationLayer.css copied successfully to CJS build.');
 });
 
 fs.copyFile('src/Page/TextLayer.css', 'dist/esm/Page/TextLayer.css', (error) => {
@@ -24,10 +24,10 @@ fs.copyFile('src/Page/TextLayer.css', 'dist/esm/Page/TextLayer.css', (error) => 
   console.log('TextLayer.css copied successfully to ESM build.');
 });
 
-fs.copyFile('src/Page/TextLayer.css', 'dist/umd/Page/TextLayer.css', (error) => {
+fs.copyFile('src/Page/TextLayer.css', 'dist/cjs/Page/TextLayer.css', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('TextLayer.css copied successfully to UMD build.');
+  console.log('TextLayer.css copied successfully to CJS build.');
 });
