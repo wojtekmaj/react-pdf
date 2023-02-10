75cd00fa9da319ac2cdd3a45adc7997c7151e08d
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-02-10 14:51:14 +0100

    Migrate from Webpack to Vite

---
+++ test/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
+});
