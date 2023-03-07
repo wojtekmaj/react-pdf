6a396e2dbd085ef3ef9ec9c5b54dc95ae5683792
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-03-07 12:32:53 +0100

    Use new index files in sample and test repositories

--- sample/vite3/Sample.jsx
+++ sample/vite3/Sample.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
+import { Document, Page } from 'react-pdf/dist/esm/index.vite';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
--- test/Test.jsx
+++ test/Test.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { PDFDataRangeTransport } from 'pdfjs-dist';
-import { Document, Outline, Page } from 'react-pdf/src/entry.vite';
+import { Document, Outline, Page } from 'react-pdf/src/index.vite';
 import 'react-pdf/src/Page/AnnotationLayer.css';
 import 'react-pdf/src/Page/TextLayer.css';
 
