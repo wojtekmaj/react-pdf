df990a7f4a29291a23ba4d57af6fd9b582e71b9a
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-04-01 13:43:28 +0200

    Improve isFile propType

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { mouseEvents, touchEvents, keyboardEvents } from 'make-event-props';
+import { PDFDataRangeTransport } from 'pdfjs-dist';
 
 import { isDefined } from './utils';
 
@@ -15,13 +16,34 @@ export const eventProps = (() => {
   return result;
 })();
 
+const isTypedArray = PropTypes.oneOfType([
+  PropTypes.instanceOf(Int8Array),
+  PropTypes.instanceOf(Uint8Array),
+  PropTypes.instanceOf(Uint8ClampedArray),
+  PropTypes.instanceOf(Int16Array),
+  PropTypes.instanceOf(Uint16Array),
+  PropTypes.instanceOf(Int32Array),
+  PropTypes.instanceOf(Uint32Array),
+  PropTypes.instanceOf(Float32Array),
+  PropTypes.instanceOf(Float64Array),
+]);
+
 const fileTypes = [
   PropTypes.string,
   PropTypes.instanceOf(ArrayBuffer),
   PropTypes.shape({
-    data: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-    range: PropTypes.object,
-    url: PropTypes.string,
+    data: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.instanceOf(ArrayBuffer),
+      PropTypes.arrayOf(PropTypes.number.isRequired),
+      isTypedArray,
+    ]).isRequired,
+  }),
+  PropTypes.shape({
+    range: PropTypes.instanceOf(PDFDataRangeTransport).isRequired,
+  }),
+  PropTypes.shape({
+    url: PropTypes.string.isRequired,
   }),
 ];
 if (typeof Blob !== 'undefined') {
--- test/shared/propTypes.js
+++ test/shared/propTypes.js
@@ -1,12 +1,34 @@
 import PropTypes from 'prop-types';
+import { PDFDataRangeTransport } from 'pdfjs-dist';
+
+const isTypedArray = PropTypes.oneOfType([
+  PropTypes.instanceOf(Int8Array),
+  PropTypes.instanceOf(Uint8Array),
+  PropTypes.instanceOf(Uint8ClampedArray),
+  PropTypes.instanceOf(Int16Array),
+  PropTypes.instanceOf(Uint16Array),
+  PropTypes.instanceOf(Int32Array),
+  PropTypes.instanceOf(Uint32Array),
+  PropTypes.instanceOf(Float32Array),
+  PropTypes.instanceOf(Float64Array),
+]);
 
 const fileTypes = [
   PropTypes.string,
   PropTypes.instanceOf(ArrayBuffer),
   PropTypes.shape({
-    data: PropTypes.object,
-    range: PropTypes.object,
-    url: PropTypes.string,
+    data: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.instanceOf(ArrayBuffer),
+      PropTypes.arrayOf(PropTypes.number.isRequired),
+      isTypedArray,
+    ]).isRequired,
+  }),
+  PropTypes.shape({
+    range: PropTypes.instanceOf(PDFDataRangeTransport).isRequired,
+  }),
+  PropTypes.shape({
+    url: PropTypes.string.isRequired,
   }),
 ];
 if (typeof Blob !== 'undefined') {
