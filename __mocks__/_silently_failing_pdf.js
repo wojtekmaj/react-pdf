bedcbec3b2dc2cff61fe79961615ca268db4d374
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-04-17 10:16:59 +0200

    Remove unnecessary fingerprint from PDF mocks

--- __mocks__/_silently_failing_pdf.js
+++ __mocks__/_silently_failing_pdf.js
@@ -5,7 +5,6 @@ export default {
     fingerprint: 'a62067476e69734bb8eb60122615dfbf',
     numPages: 4,
   },
-  fingerprint: 'a62067476e69734bb8eb60122615dfbf',
   getDestination: () =>
     new Promise((resolve, reject) =>
       reject(new RenderingCancelledException('Cancelled', 'cancelled')),
