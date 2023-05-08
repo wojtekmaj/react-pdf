ab5ecd8d4b2e77ea1385c4ee061e64d396892a13
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-08 15:00:23 +0200

    Fix test suite using built version

--- test/Test.tsx
+++ test/Test.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { PDFDataRangeTransport } from 'pdfjs-dist';
-import { pdfjs, Document, Outline, Page } from 'react-pdf';
+import { pdfjs, Document, Outline, Page } from 'react-pdf/src';
 import 'react-pdf/src/Page/AnnotationLayer.css';
 import 'react-pdf/src/Page/TextLayer.css';
 
