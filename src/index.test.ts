a5d34cc5eaf9d86570766db1ce8b38ecc10e198d
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-06-30 11:19:20 +0200

    Make test entry export the same things main entry exports

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,28 @@
 import * as pdfjs from 'pdfjs-dist';
+
 import Document from './Document';
 import Outline from './Outline';
 import Page from './Page';
+import Thumbnail from './Thumbnail';
+
+import useDocumentContext from './shared/hooks/useDocumentContext';
+import useOutlineContext from './shared/hooks/useOutlineContext';
+import usePageContext from './shared/hooks/usePageContext';
+
+export type { DocumentProps } from './Document';
+export type { OutlineProps } from './Outline';
+export type { PageProps } from './Page';
+export type { ThumbnailProps } from './Thumbnail';
 
 import './pdf.worker.entry';
 
-export { pdfjs, Document, Outline, Page };
+export {
+  pdfjs,
+  Document,
+  Outline,
+  Page,
+  Thumbnail,
+  useDocumentContext,
+  useOutlineContext,
+  usePageContext,
+};
