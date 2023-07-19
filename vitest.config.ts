f2e7f65d22246b162c1806315edf9dc27fb1d47c
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-19 13:31:05 +0200

    Suppress warnings about missing CSS in unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
       inline: ['vitest-canvas-mock'],
     },
     environment: 'jsdom',
+    environmentOptions: {
+      jsdom: {
+        html: '<!DOCTYPE html><html><body style="--react-pdf-annotation-layer: 1;--react-pdf-text-layer: 1;"></body></html>',
+      },
+    },
     exclude: [...configDefaults.exclude, 'src/index.test.ts'],
     setupFiles: 'vitest.setup.ts',
     threads: false,
