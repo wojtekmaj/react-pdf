c8dce0c90825bf0b3b01aa98e4ed5370eda3fcd9
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-19 13:32:05 +0200

    Add copy-cmaps and copy-standard-fonts scripts to Vite 3 and Vite 4 samples

---
+++ sample/vite4/scripts/copy-standard-fonts.ts
@@ -0,0 +1,20 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+function copyDir(from: string, to: string) {
+  // Ensure target directory exists
+  fs.mkdirSync(to, { recursive: true });
+
+  const files = fs.readdirSync(from);
+  files.forEach((file) => {
+    fs.copyFileSync(path.join(from, file), path.join(to, file));
+  });
+}
+
+const standardFontsDir = path.join(
+  path.dirname(require.resolve('pdfjs-dist/package.json')),
+  'standard_fonts',
+);
+const targetDir = path.join('dist', 'standard_fonts');
+
+copyDir(standardFontsDir, targetDir);
