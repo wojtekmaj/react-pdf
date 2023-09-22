381c1b6efcd90d5bad4557cc5b268aa78972dc7b
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-22 12:00:49 +0200

    Remove .js extension from import since CRA5 does not support it
    Related to #1606

--- sample/create-react-app-5/src/index.tsx
+++ sample/create-react-app-5/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Sample from './Sample.js';
+import Sample from './Sample';
 
 createRoot(document.getElementById('react-root')!).render(<Sample />);
