45d0d39f6abfe99bcc7dfb90db3426a19dc21785
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-30 20:38:45 +0200

    Improve Next.js Pages Router sample
    Related to #1620

--- sample/next-pages/next.config.js
+++ sample/next-pages/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    /**
+     * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
+     * Module parse failed: Unexpected character '�' (1:0)" error
+     */
+    config.resolve.alias.canvas = false;
+
     // You may not need this, it's just to support moduleResolution: 'node16'
     config.resolve.extensionAlias = {
       '.js': ['.js', '.ts', '.tsx'],
