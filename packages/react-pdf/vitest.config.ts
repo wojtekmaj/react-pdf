f5663201c225e691fc8fd9df46b0c530e478ba21
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-03 12:05:26 +0200

    Set required CSS variables in DOM library-agnostic way

--- packages/react-pdf/vitest.config.ts
+++ packages/react-pdf/vitest.config.ts
@@ -3,11 +3,6 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    environmentOptions: {
-      jsdom: {
-        html: '<!DOCTYPE html><html><body style="--react-pdf-annotation-layer: 1;--react-pdf-text-layer: 1;"></body></html>',
-      },
-    },
     exclude: [...configDefaults.exclude, 'src/index.test.ts'],
     server: {
       deps: {
--- packages/react-pdf/vitest.setup.ts
+++ packages/react-pdf/vitest.setup.ts
@@ -8,3 +8,6 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+document.body.style.setProperty('--react-pdf-annotation-layer', '1');
+document.body.style.setProperty('--react-pdf-text-layer', '1');
