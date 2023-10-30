406571325897233b3db40dfd4eec2771088f0a7b
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-30 21:37:57 +0100

    Update @testing-library/jest-dom to 6.0.0

--- packages/react-pdf/vitest.setup.ts
+++ packages/react-pdf/vitest.setup.ts
@@ -1,9 +1,7 @@
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-dom/vitest';
 import 'vitest-canvas-mock';
-
-expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
