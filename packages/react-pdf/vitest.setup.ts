2ba89d8cb968af6e522e688329cbf2e412b80462
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-04-30 13:09:54 +0200

    Add Promise.withResolvers polyfill

--- packages/react-pdf/vitest.setup.ts
+++ packages/react-pdf/vitest.setup.ts
@@ -1,6 +1,7 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+import 'core-js/proposals/promise-with-resolvers';
 
 afterEach(() => {
   cleanup();
