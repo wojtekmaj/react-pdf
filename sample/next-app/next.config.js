af1bd00ac6edd80cfa642f3d4052bf68307e01d2
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-06-13 10:28:26 +0200

    Remove unnecessary Next.js workaround

--- sample/next-app/next.config.js
+++ sample/next-app/next.config.js
@@ -19,12 +19,6 @@ const nextConfig = {
    */
   swcMinify: false,
   webpack: (config) => {
-    /**
-     * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
-     * Module parse failed: Unexpected character '�' (1:0)" error
-     */
-    config.resolve.alias.canvas = false;
-
     // You may not need this, it's just to support moduleResolution: 'node16'
     config.resolve.extensionAlias = {
       '.js': ['.js', '.ts', '.tsx'],
