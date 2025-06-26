afde065a48b7697ecc77975be7d02367e5000dd6
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-06-26 13:41:35 +0200

    Import stylesheets from recommended paths

--- sample/webpack5/Sample.tsx
+++ sample/webpack5/Sample.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useId, useState } from 'react';
 import { useResizeObserver } from '@wojtekmaj/react-hooks';
 import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 import './Sample.css';
 
