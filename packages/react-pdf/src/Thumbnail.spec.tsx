06a2284895ef1485bea85596e527246adc3b074a
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-08-05 10:37:52 +0200

    Add missing unit test for Thumbnail component

--- packages/react-pdf/src/Thumbnail.spec.tsx
+++ packages/react-pdf/src/Thumbnail.spec.tsx
@@ -5,6 +5,7 @@ import { fireEvent, render } from '@testing-library/react';
 import { pdfjs } from './index.test.js';
 
 import Thumbnail from './Thumbnail.js';
+import LinkService from './LinkService.js';
 
 import failingPdf from '../../../__mocks__/_failing_pdf.js';
 import silentlyFailingPdf from '../../../__mocks__/_silently_failing_pdf.js';
@@ -48,6 +49,8 @@ describe('Thumbnail', () => {
   const desiredLoadedThumbnail: Partial<PDFPageProxy> = {};
   const desiredLoadedThumbnail2: Partial<PDFPageProxy> = {};
   const desiredLoadedThumbnail3: Partial<PDFPageProxy> = {};
+
+  const linkService = new LinkService();
 
   beforeAll(async () => {
     pdf = await pdfjs.getDocument({ data: pdfFile.arrayBuffer }).promise;
@@ -615,10 +618,27 @@ describe('Thumbnail', () => {
     expect(page.height).toEqual(page.originalHeight * (page.width / page.originalWidth));
   });
 
+  it('calls onClick callback when clicked a page (sample of mouse events family)', () => {
+    const onClick = vi.fn();
+
+    const { container } = renderWithContext(<Thumbnail onClick={onClick} />, {
+      linkService,
+      pdf,
+    });
+
+    const page = container.querySelector('.react-pdf__Thumbnail__page') as HTMLDivElement;
+    fireEvent.click(page);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
   it('calls onTouchStart callback when touched a page (sample of touch events family)', () => {
     const onTouchStart = vi.fn();
 
-    const { container } = renderWithContext(<Thumbnail onTouchStart={onTouchStart} />, { pdf });
+    const { container } = renderWithContext(<Thumbnail onTouchStart={onTouchStart} />, {
+      linkService,
+      pdf,
+    });
 
     const page = container.querySelector('.react-pdf__Thumbnail__page') as HTMLDivElement;
     fireEvent.touchStart(page);
