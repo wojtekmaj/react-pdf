29738e66bcf1d5fe513130873f87dc32bf321056
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-12-04 11:45:19 +0100

    Minor formatting

--- sample/next-pages/pages/Sample.tsx
+++ sample/next-pages/pages/Sample.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useId, useState } from 'react';
 import { useResizeObserver } from '@wojtekmaj/react-hooks';
-import { pdfjs, Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
